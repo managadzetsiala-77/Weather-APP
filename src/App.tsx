f91209c539aeb1ae407629ebd33c870946ec4020
student <student@local.emis.ge>
2026-06-01 14:37:37 +0400

    corect skileton

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,8 @@ export default function App() {
 
   const [windSpeed, setWindSpeed] = useState("km");
 
+  const text = ["fills like: ", "humidity:", "wind:", "precipitation:"];
+
   return (
     <div>
       <Header />
@@ -22,88 +24,76 @@ export default function App() {
 
       <SearchBar setCity={setCity} setWeather={setWeather} />
 
-<div className="flex gap-5  max-w-300 mx-auto mt-10">
-  <div className="w-full">
-
-    {!weather ? (
-      <>
-        {/* Today skeleton */}
-        <div className="
+      <div className="flex gap-5  max-w-300 mx-auto mt-10">
+        <div className="w-full">
+          {!weather ? (
+            <>
+              {/* Today skeleton */}
+              <div
+                className="
           w-full
-          h-[220px]
+          h-65
           rounded-3xl
           bg-[#24243D]
           animate-pulse
           flex
           items-center
           justify-center
-        ">
-          <div className="text-gray-300">
-            ● ● ●
-            <p className="text-center mt-2">Loading...</p>
-          </div>
+        "
+              >
+                <div className="text-gray-300 ">
+                  ● ● ●<p className="text-center mt-2">Loading...</p>
+                </div>
+              </div>
+
+              {/* Stats skeleton */}
+              <div className="grid grid-cols-4 gap-4 mt-6">
+                {[0, 1, 2, 3].map((i) => (
+                  <div
+                    key={i}
+                    className="h-30 rounded-xl bg-[#24243D] animate-pulse flex flex-col gap-4 p-4"
+                  >
+                    <p>{text[i]}</p>
+                  --
+                  </div>
+                ))}
+              </div>
+
+              {/* Daily skeleton */}
+              <h2 className="mt-10 mb-4 font-bold">Daily forecast</h2>
+
+              <div className="grid grid-cols-7 gap-4 h-2">
+                {[1, 2, 3, 4, 5, 6, 7].map((i) => (
+                  <div
+                    key={i}
+                    className="h-50 rounded-xl bg-[#24243D] animate-pulse"
+                  ></div>
+                ))}
+              </div>
+            </>
+          ) : (
+            <>
+              <Today city={city} weather={weather} />
+
+              <TodaysList weather={weather} windSpeed={windSpeed} />
+
+              <div className="mt-16">
+                <h2 className="text-2xl font-bold mb-5">Daily forecast</h2>
+
+                <div className="flex gap-3">
+                  {weather.daily.time.map((date, i) => (
+                    <DailyCard key={date} weather={weather} date={date} i={i} />
+                  ))}
+                </div>
+              </div>
+            </>
+          )}
         </div>
 
-        {/* Stats skeleton */}
-        <div className="grid grid-cols-4 gap-4 mt-6">
-          {[1,2,3,4].map(i=>(
-            <div
-              key={i}
-              className="h-[90px] rounded-xl bg-[#24243D] animate-pulse"
-            />
-          ))}
-        </div>
-
-        {/* Daily skeleton */}
-        <h2 className="mt-10 mb-4 font-bold">
-          Daily forecast
-        </h2>
-
-        <div className="grid grid-cols-7 gap-4">
-          {[1,2,3,4,5,6,7].map(i=>(
-            <div
-              key={i}
-              className="h-[120px] rounded-xl bg-[#24243D] animate-pulse"
-            />
-          ))}
-        </div>
-      </>
-    ) : (
-      <>
-        <Today city={city} weather={weather} />
-
-        <TodaysList
-          weather={weather}
-          windSpeed={windSpeed}
-        />
-
-        <div className="mt-16">
-          <h2 className="text-2xl font-bold mb-5">
-            Daily forecast
-          </h2>
-
-          <div className="flex gap-3">
-            {weather.daily.time.map((date, i) => (
-              <DailyCard
-                key={date}
-                weather={weather}
-                date={date}
-                i={i}
-              />
-            ))}
-          </div>
-        </div>
-      </>
-    )}
-  </div>
-
-  <section className="w-[30%]">
-    <HourlyList weather={weather} />
-  </section>
-
-</div>
-      
-    
+        <section className="w-[30%]">
+          <HourlyList weather={weather} />
+        </section>
+      </div>
 
       <button className="border p-1.5" onClick={() => setWindSpeed("km")}>
         km/h
@@ -112,10 +102,7 @@ export default function App() {
         mp/h
       </button>
 
-      <div>
-      
-        
-      </div>
+      <div></div>
     </div>
   );
 }
